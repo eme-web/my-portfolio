4c22446e672303a4392c472a24b28c84c8f2aa7a
eme-web <udobongeme4@gmail.com>
2025-06-20 15:04:18 +0100

    updated resume

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,80 +1,97 @@
 import {
-    faBarsStaggered,
-    faCode,
-    faXmark,
+  faBarsStaggered,
+  faCode,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import resume from "../Assets/EmeUdobong_CV.pdf";
+import resume from "../Assets/EmeUdobong-FS.pdf";
 import { useState } from "react";
 
-
 export default function Navbar() {
-    const [responsivenavbar, setResponsiveNavbar] = useState(false);
+  const [responsivenavbar, setResponsiveNavbar] = useState(false);
 
-    function handleNavbar(){
-        return setResponsiveNavbar(!responsivenavbar);
-    }
+  function handleNavbar() {
+    return setResponsiveNavbar(!responsivenavbar);
+  }
 
-    const navbar = [
-        {
-            id: 1,
-            liName: "About",
-            link: "#about",
-        },
-        {
-            id: 2,
-            liName: "Skills",
-            link: "#skills",
-        },
-        {
-            id: 3,
-            liName: "Projects",
-            link: "#projects",
-        },
-        {
-            id: 4,
-            liName: "Contact",
-            link: "#contact",
-        },
-    ];
+  const navbar = [
+    {
+      id: 1,
+      liName: "About",
+      link: "#about",
+    },
+    {
+      id: 2,
+      liName: "Skills",
+      link: "#skills",
+    },
+    {
+      id: 3,
+      liName: "Projects",
+      link: "#projects",
+    },
+    {
+      id: 4,
+      liName: "Contact",
+      link: "#contact",
+    },
+  ];
 
-    return (
-        <header className="px-12 py-4 fixed left-0 right-0 mx-auto bg-gray-900 shadow-2xl z-50 md:backdrop-blur-xl md:bg-opacity-70">
-            <nav className="container mx-auto flex justify-between items-center">
-                <h2 className="cursor-default text-3xl">
-                    <FontAwesomeIcon icon={faCode} />
-                </h2>
-                <ul className={(responsivenavbar ? "left-0" : "left-full") +
-                    "fixed buttom-0 top-20 bg-gray-900 bg-opacity-95 md:bg-transpparent w-10/12 px-4 py-6 md:py-0 space-y-5 md:static md:flex md:space-y-0 md:gap-8 justify-center text-md font-bold xl:text-lg transition-left duration-200"}>
-                        {navbar.map((navbarElement) => {
-                            return (
-                                <li key={navbarElement.id} 
-                                className="hover:md:translate-y-1 duration-100">
-                                    <a className="hover:text-cyan-500 duration-100" href={navbarElement.link}>{navbarElement.liName}</a>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                    
-                    <div className="flex gap-8 items-center">
-                        <a href={resume} alt="Eme Udobong Resume" target="blank"
-                        className="px-6 py-3 border rounded hover:rounded-lg hover:bg-cyan-800 duration-200" >Resume</a>
-                        {responsivenavbar ? (
-                            <button className="md:hidden" onClick={handleNavbar}>
-                                <FontAwesomeIcon 
-                                  icon={faXmark}
-                                  className="text-4xl cursor-pointer hover:text-red-600 duration-100"
-                                  />
-                            </button>
-                        ) : (
-                            <button className="md:hidden" onClick={handleNavbar}>
-                                <FontAwesomeIcon icon={faBarsStaggered}
-                                className="text-4xl cursor-pointer hover:text-cyan-600 duration-100" />
-                            </button>
-                        )}
-                    </div>
-            </nav>
-        </header>
+  return (
+    <header className="px-12 py-4 fixed left-0 right-0 mx-auto bg-gray-900 shadow-2xl z-50 md:backdrop-blur-xl md:bg-opacity-70">
+      <nav className="container mx-auto flex justify-between items-center">
+        <h2 className="cursor-default text-3xl">
+          <FontAwesomeIcon icon={faCode} />
+        </h2>
+        <ul
+          className={
+            (responsivenavbar ? "left-0" : "left-full") +
+            "fixed buttom-0 top-20 bg-gray-900 bg-opacity-95 md:bg-transpparent w-10/12 px-4 py-6 md:py-0 space-y-5 md:static md:flex md:space-y-0 md:gap-8 justify-center text-md font-bold xl:text-lg transition-left duration-200"
+          }
+        >
+          {navbar.map((navbarElement) => {
+            return (
+              <li
+                key={navbarElement.id}
+                className="hover:md:translate-y-1 duration-100"
+              >
+                <a
+                  className="hover:text-cyan-500 duration-100"
+                  href={navbarElement.link}
+                >
+                  {navbarElement.liName}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
 
-    );
+        <div className="flex gap-8 items-center">
+          <a
+            href={resume}
+            alt="Eme Udobong Resume"
+            target="blank"
+            className="px-6 py-3 border rounded hover:rounded-lg hover:bg-cyan-800 duration-200"
+          >
+            Resume
+          </a>
+          {responsivenavbar ? (
+            <button className="md:hidden" onClick={handleNavbar}>
+              <FontAwesomeIcon
+                icon={faXmark}
+                className="text-4xl cursor-pointer hover:text-red-600 duration-100"
+              />
+            </button>
+          ) : (
+            <button className="md:hidden" onClick={handleNavbar}>
+              <FontAwesomeIcon
+                icon={faBarsStaggered}
+                className="text-4xl cursor-pointer hover:text-cyan-600 duration-100"
+              />
+            </button>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
 }
--- src/data.js
+++ src/data.js
@@ -1,136 +1,144 @@
 import {
-    faMdb,
-    faGitAlt,
-    faPython,
-    faJs,
-    faNode,
-    faNodeJS,
-    faNodeJs
-  } from "@fortawesome/free-brands-svg-icons";
+  faMdb,
+  faGitAlt,
+  faPython,
+  faJs,
+  faNode,
+  faNodeJS,
+  faNodeJs,
+  faPhp,
+  faLaravel,
+} from "@fortawesome/free-brands-svg-icons";
 import img1 from "./Images/RealtorPage.png";
 import img2 from "./Images/codeBookPage.png";
 import img3 from "./Images/cinemaHomePage.png";
-import img4 from "./Images/Omnifood.png"
+import img4 from "./Images/Omnifood.png";
 import img5 from "./Images/Decamper.png";
 import img6 from "./Images/Blogging-api.png";
 
 const aboutMe = [
-    {
-        id: 1,
-        para: "My name is Eme Udobong and I love design and build things for the web.",
-    },
+  {
+    id: 1,
+    para: "My name is Eme Udobong and I love design and build things for the web.",
+  },
 ];
 
 const education = [
-    {
-        id: 1,
-        schoolName: "University of Calabar",
-        specialization: "B.Sc Public Health",
-        class: "Second Class Upper",
-        year: 2018,
-    },
+  {
+    id: 1,
+    schoolName: "University of Calabar",
+    specialization: "B.Sc Public Health",
+    class: "Second Class Upper",
+    year: 2018,
+  },
 ];
 
 const skills = [
-    {
-        id: "express",
-        name: "Express",
-        icon: faNodeJs,
-    },
-    {
-        id: "JavaScript",
-        name:"JavaScript",
-        icon: faJs,
-       
-    },
-    {
-        id: "python",
-        name: "Python",
-        icon: faPython,
-
-    },
-    {
-        id: "nodejs",
-        name: "Node.js",
-        icon: faNode,
-
-    },
-    {
-        id: "mongo",
-        name: "MongoDB",
-        icon: faMdb,
-    },
-    {
-        id: "git",
-        name:"Git",
-        icon: faGitAlt,
-    }
+  {
+    id: "express",
+    name: "Express",
+    icon: faNodeJs,
+  },
+  {
+    id: "JavaScript",
+    name: "JavaScript",
+    icon: faJs,
+  },
+  {
+    id: "Php",
+    name: "PHP",
+    icon: faPhp,
+  },
+  {
+    id: "Laravel",
+    name: "LARAVEL",
+    icon: faLaravel,
+  },
+  {
+    id: "nodejs",
+    name: "Node.js",
+    icon: faNode,
+  },
+  {
+    id: "mongo",
+    name: "MongoDB",
+    icon: faMdb,
+  },
+  {
+    id: "git",
+    name: "Git",
+    icon: faGitAlt,
+  },
 ];
 
 const projects = [
-    {
-        id: 1,
-        imgSrc: img1,
-        alt: "Realtor-Clone - Real Estate platform",
-        projectName: "Realtor Clone - Real Estate Application",
-        technologies: "React.js Tailwindcss, Firebase",
-        description: "Realtor-Clone is a real estate platform where users can buy, sell, and rent properties in the most desirable neigborhood.",
-        projectLink: "https://realtor-clone-lilac.vercel.app/",
-        githubLink: "https://github.com/eme-web/Realtor-clone",
-    },
-    {
-        id: 2,
-        imgSrc: img2,
-        alt: "CodeBook - E-commerce platform",
-        projectName: "CodeBook - E-commerce Application",
-        technologies: "React.js, Tailwind CSS, JSON Server, JWTAuth, Context API",
-        description: "An E-commerce application where a user can search for a coding book of choice and make purchase.",
-        projectLink: "https://codebook-snowy.vercel.app/",
-        githubLink:"https://github.com/eme-web/codebook",
-    },
-    {
-        id: 3,
-        imgSrc: img3,
-        alt: "Cinema - A Movie web application",
-        projectName: "Cinema - A Movie Web Application",
-        technologies: "React.js, Tailwind CSS, TMDB API ",
-        description: "A web application where users can have access to search and watch movie of their choice.",
-        projectLink:"https://cinema-nu-six.vercel.app/",
-        githubLink: "https://github.com/eme-web/cinema",
-    },
-    {
-        id: 4,
-        imgSrc: img4,
-        alt: "Omnifood - A Meal web application",
-        projectName: "Omnifood - A meal Web Application",
-        technologies: "Html, Css ",
-        description: "A web application where users can have a meal plan, get food recipes and also order food.",
-        projectLink:"https://foodie-six-mocha.vercel.app/",
-        githubLink: "https://github.com/eme-web/Foodie",
-    },
-    {
-        id: 5,
-        imgSrc: img5,
-        alt: "Tech-BootcampAPI",
-        projectName: "Tech-Bootcamp",
-        technologies: "Express, Node.js, MongoDB, JWT",
-        description: "Backend API for Tech-Bootcamp application to manage bootcamps, courses, reviews, users and authentication.",
-        projectLink:"https://bootcamper-api-onqm.onrender.com/",
-        githubLink: "https://github.com/eme-web/TechBootcamperAPI.git",
-    },
-    {
-        id: 6,
-        imgSrc: img6,
-        alt: "BloggingAPI",
-        projectName: "Blogging Platform",
-        technologies: "Express, Node.js, MongoDB, JWT",
-        description: "Backend API for Blogging application to manage blog posts, users and authentication.",
-        projectLink:"https://blogging-api-295u.onrender.com/",
-        githubLink: "https://github.com/eme-web/Blogging-api.git",
-    },
-   
-
+  {
+    id: 1,
+    imgSrc: img1,
+    alt: "Realtor-Clone - Real Estate platform",
+    projectName: "Realtor Clone - Real Estate Application",
+    technologies: "React.js Tailwindcss, Firebase",
+    description:
+      "Realtor-Clone is a real estate platform where users can buy, sell, and rent properties in the most desirable neigborhood.",
+    projectLink: "https://realtor-clone-lilac.vercel.app/",
+    githubLink: "https://github.com/eme-web/Realtor-clone",
+  },
+  {
+    id: 2,
+    imgSrc: img2,
+    alt: "CodeBook - E-commerce platform",
+    projectName: "CodeBook - E-commerce Application",
+    technologies: "React.js, Tailwind CSS, JSON Server, JWTAuth, Context API",
+    description:
+      "An E-commerce application where a user can search for a coding book of choice and make purchase.",
+    projectLink: "https://codebook-snowy.vercel.app/",
+    githubLink: "https://github.com/eme-web/codebook",
+  },
+  {
+    id: 3,
+    imgSrc: img3,
+    alt: "Cinema - A Movie web application",
+    projectName: "Cinema - A Movie Web Application",
+    technologies: "React.js, Tailwind CSS, TMDB API ",
+    description:
+      "A web application where users can have access to search and watch movie of their choice.",
+    projectLink: "https://cinema-nu-six.vercel.app/",
+    githubLink: "https://github.com/eme-web/cinema",
+  },
+  {
+    id: 4,
+    imgSrc: img4,
+    alt: "Omnifood - A Meal web application",
+    projectName: "Omnifood - A meal Web Application",
+    technologies: "Html, CSS",
+    description:
+      "A web application where users can have a meal plan, get food recipes and also order food.",
+    projectLink: "https://foodie-six-mocha.vercel.app/",
+    githubLink: "https://github.com/eme-web/Foodie",
+  },
+  {
+    id: 5,
+    imgSrc: img5,
+    alt: "Tech-BootcampAPI",
+    projectName: "Tech-Bootcamp",
+    technologies: "Express, Node.js, MongoDB, JWT",
+    description:
+      "Backend API for Tech-Bootcamp application to manage bootcamps, courses, reviews, users and authentication.",
+    projectLink: "https://bootcamper-api-onqm.onrender.com/",
+    githubLink: "https://github.com/eme-web/TechBootcamperAPI.git",
+  },
+  {
+    id: 6,
+    imgSrc: img6,
+    alt: "BloggingAPI",
+    projectName: "Blogging Platform",
+    technologies: "Express, Node.js, MongoDB, JWT",
+    description:
+      "Backend API for Blogging application to manage blog posts, users and authentication.",
+    projectLink: "https://blogging-api-295u.onrender.com/",
+    githubLink: "https://github.com/eme-web/Blogging-api.git",
+  },
 ];
 
-export { aboutMe, education, skills};
+export { aboutMe, education, skills };
 export default projects;
