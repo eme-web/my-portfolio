b7796429698c49560417414ef897c68d1baae616
eme-web <udobongeme4@gmail.com>
2025-06-20 15:15:47 +0100

    updated work-experience

--- src/utils/work-experience.js
+++ src/utils/work-experience.js
@@ -1,23 +1,25 @@
 export const workExperience = [
-    {
-        id: "Skyway",
-        position: "Backend Developer",
-        organization: "Skyway Aviation Handling Company",
-        duration: "Sep 2023 - Present",
-        technologies: [
-            "Express",
-            "Node.js",
-            "MongoDB",
-            "JavaScript",
-        ],
-        description: [
-            "Improved performance and reliability of the system by breaking down the monolith into various microservices.",
-            "Built, documented new features and APIs and support frontend engineers with API integrations.",
-            "Developed customer-facing applications using RESTful, WebSocket and RPC-based APIs.", 
-            "Created database schemas that represent and support new products/enhancements.",
-            "Maintained code integrity, organization, versioning and applied object-oriented design principles.",
-            "Debugged and resolved production issues as required.", 
-       
-        ],
-    },
+  {
+    id: "Skyway",
+    position: "Software Engineer",
+    organization: "Skyway Aviation Handling Company",
+    duration: "Jan 2024 - Present",
+    technologies: [
+      "Node.js",
+      "React.js",
+      "Typescript",
+      "PHP",
+      "Laravel",
+      "MongoDB",
+      "MySQL",
+    ],
+    description: [
+      "Improved performance and reliability of the system by breaking down the monolith into various microservices.",
+      "Built and integrated RESTful APIs and real-time features (WebSocket) used by frontend apps",
+      "Developed internal tools for flight operations, budgeting, and user management using RESTful and WebSocket.",
+      "Integrated third-party APIs and implemented secure authentication/authorization systems.",
+      "Maintained code integrity, organization, versioning and applied object-oriented design principles.",
+      "Participated in code reviews and Agile sprints, contributing to overall team velocity.",
+    ],
+  },
 ];
